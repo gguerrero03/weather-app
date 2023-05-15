1a05ae248ab95a4c64aefccd44578e31288226cc
Giannella Guerrero <gvguerre@ucsc.edu>
2023-05-14 23:57:40 -0700

    completed

--- script.js
+++ script.js
@@ -23,6 +23,30 @@ function success(position){
 }
 const btn = document.getElementById('getWeatherBtn');
 btn.addEventListener("click", function(){
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", `localhost:3000/weather/${latitude}/${longitude}`); // opens a get request
+  xhr.send(); // sends the request
+
+  xhr.onload = function() {
+    const body = JSON.parse(xhr.responseText)
+    let temperature = body.temperature;
+    let weatherStatus = body.weatherStatus;
+    document.getElementById("temperature").innerHTML = `temperature: ${temperature}°F`;
+    document.getElementById("weatherStatus").innerHTML = `weatherStatus: ${weatherStatus}`;
+  }
+  const xhr2 = new XMLHttpRequest();
+  xhr2.open("Get", `localhost:3000/forecast/${latitiude}/${longitude}`);
+  xhr2.send();
+
+  xhr2.onload = function() {
+    const body = JSON.parse(xhr2.responseText)
+    let forecast = body.forecast;
+    let forecastElements = document.getElementsByClassName("forecast");
+    for (let i = 0; i < forecast.length; i++) {
+      forecastElements[i].innerHTML = `${forecast[i].dayName}: ${forecast[i].temp}°F`;
+    }
+}
+
   let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]] // Nested array of redefines day/forecast pairs
   let forecastElements = document.getElementsByClassName("forecast"); // setting forecastElement to a array of divs with 
   for (let i = 0; i < forecast.length; i++) { // For loop that goes from the 0th index to the length - 1 index 
